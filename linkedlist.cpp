33afd29e5246340463be9d8535e976823842fec1
rushtheboy <rushroy1999@gmail.com>
2020-08-03 18:05:23 +0530

    insertion at position not working, reverse with map working

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 struct node{
     int data;
@@ -75,46 +76,6 @@ class linkedlist{
         }
     }
 
-    void add_at_position(){  //assuming indexing starts at 1
-        int pos = 0, i =1, val =0;
-        node *temp = new node;
-        node *ins = new node;
-        std::cout << "Enter the position at which you want to insert the node(indexing starts at 1): ";
-        std::cin >> pos;
-        if(pos>(count+1) || pos < 1){
-            std::cout << "Invalid position.\n";
-            std::cout << "Length of list is: " << count << "\n";
-        }
-        else{
-            std::cout << "Enter the value you'd like to insert: ";
-            std::cin >> val;
-            if(count == 0){   //empty list
-                head = temp;  
-                tail = head;
-                count++;
-            }
-            else if(count == 1){  //list with one node
-                temp = head;
-                temp->data = val;
-                temp->next = NULL;
-                head->next = temp;  //head's next now points to temp, so temp is at 2nd position
-                tail = temp;   //tail is updated to temp;
-                count++;
-            }
-            else if(count > 1){  //list with multiple nodes
-                temp = head;
-                while(i < pos){   //get temp to the position
-                    temp = temp->next;
-                    i++;
-                }
-                ins->data = val;
-                ins->next = temp;
-                temp = ins;
-                count++;
-            }
-        }
-    }
-
     void delete_head(){
         node *temp = new node;
         if(head == NULL)   //empty list
@@ -159,21 +120,38 @@ class linkedlist{
         }
     }
 
+    void reverse(){
+        node *itr = new node;
+        int reversecount = 1;
+        std::unordered_map<int, int> reversekey;  //we store the position of each node with the value of each node
+        itr = head;
+        if(count==0){
+            std::cout << "Nothing to reverse, list empty.\n";
+        }
+        else if(count == 1){
+            std::cout << head->data << "\n";
+        }
+        else{
+            while(itr != NULL){
+                reversekey.insert(std::make_pair(reversecount,itr->data));  
+                itr = itr->next;
+                reversecount++;
+            }
+            int i;
+            for(i = (reversecount-1); i >0; i--){
+                std::cout << reversekey.at(i) << "\n";
+            }
+        }
+    }
+
 };
 
 int main(){
     linkedlist a;
-    a.add_to_start(2);
-    a.add_to_start(1);
-    a.add_to_end(3);
-    a.add_to_end(4);
+    a.add_to_start(3);
+    a.add_to_start(4);
+    a.add_to_start(5);
     a.display();
-    a.delete_head();
-    a.display();
-    a.delete_tail();
-    a.display();
-    a.add_at_position();
-    a.display();
-    a.add_at_position();
-    a.display();
+    a.reverse();
+    //a.display();
 }
